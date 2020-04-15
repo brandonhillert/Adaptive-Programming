20dca1353c591a6763cb8d89a0630d70b3a4fae8
Brandon Hillert <brandon.hillert@student.hu.nl>
2020-04-15 14:28:00 +0200

    Class waterfles

---
+++ Les 1/src/ObjectOrientedProgamming/Main.java
@@ -0,0 +1,14 @@
+package ObjectOrientedProgamming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello");
+
+	waterfles fles = new waterfles("Spa", "rood", 70);
+	System.out.println(fles);
+
+
+
+    }
+}
---
+++ Les 1/src/ObjectOrientedProgamming/waterfles.java
@@ -0,0 +1,17 @@
+package ObjectOrientedProgamming;
+
+public class waterfles {
+
+    private String merk;
+    private String kleur;
+    private int inhoud;
+
+    public waterfles(String merk,String kleur, int inhoud ) {
+        this.merk = merk;
+        this.kleur = kleur;
+        this.inhoud = inhoud;
+    }
+
+
+
+}
